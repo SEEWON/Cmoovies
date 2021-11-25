7df5a5d0d4c488b2b17c25c30a8e8a58d50aceb6
SEEWON <andyaaa78@gmail.com>
2021-11-25 16:26:18 +0900

    [3일차] API fetching + Swiper 추가

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,9 +1,71 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Dimensions, StyleSheet } from 'react-native';
+import Swiper from 'react-native-web-swiper';
+import styled from 'styled-components/native';
+import { makeImgPath } from '../utils';
+import { BlurView } from 'expo-blur';
 
-const Movies = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Movies</Text>
-  </View>
-);
+const API_KEY = '95d088d12c9645bd0c69bc51a80df3a1';
+
+const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+//es6, same as SCREEN_HEIGHT = Dimensions.get('window').height;
+
+const Movies = () => {
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const getNowPlaying = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+    );
+    const json = await response.json();
+    setNowPlaying(json.results);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+  return loading ? (
+    <Loader>
+      <ActivityIndicator size="large" color="#0000ff" />
+    </Loader>
+  ) : (
+    <Container>
+      <Swiper
+        loop
+        timeout={3.5}
+        controlsEnabled={false}
+        containerStyle={{ width: '100%', height: SCREEN_HEIGHT / 4 }}
+      >
+        {nowPlaying.map((movie) => (
+          <View key={movie.id}>
+            <BgImg
+              style={StyleSheet.absoluteFill}
+              source={{ uri: makeImgPath(movie.backdrop_path) }}
+            ></BgImg>
+            <BlurView intensity={100} style={StyleSheet.absoluteFill}>
+              <Title>{movie.original_title}</Title>
+            </BlurView>
+          </View>
+        ))}
+      </Swiper>
+    </Container>
+  );
+};
+
+const Container = styled.ScrollView``;
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BgImg = styled.Image``;
+
+const Title = styled.Text``;
+
 export default Movies;
