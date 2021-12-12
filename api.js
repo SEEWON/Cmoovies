79d088c26b38cb462f2dc4a4066f5e6150ab1545
SEEWON <andyaaa78@gmail.com>
2021-12-13 02:04:16 +0900

    [8일차] Fetching youtube links, add Linking&Sharing

--- api.js
+++ api.js
@@ -14,10 +14,16 @@ export const moviesApi = {
     fetch(
       `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=ko&page=1&region=KR`
     ).then((res) => res.json()),
-  search: (queryKey) => {
+  search: ({ queryKey }) => {
     const [_, query] = queryKey;
     return fetch(
       `${BASE_URL}/search/movie?api_key=${API_KEY}&language=ko&query=${query}&page=1`
+    ).then((res) => res.json());
+  },
+  detail: ({ queryKey }) => {
+    const [_, id] = queryKey;
+    return fetch(
+      `${BASE_URL}/movie/${id}?api_key=${API_KEY}&append_to_response=videos,images`
     ).then((res) => res.json());
   },
 };
@@ -35,10 +41,16 @@ export const tvApi = {
     fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}`).then((res) =>
       res.json()
     ),
-  search: (queryKey) => {
+  search: ({ queryKey }) => {
     const [_, query] = queryKey;
     return fetch(
       `${BASE_URL}/search/tv?api_key=${API_KEY}&language=ko&query=${query}&page=1`
+    ).then((res) => res.json());
+  },
+  detail: ({ queryKey }) => {
+    const [_, id] = queryKey;
+    return fetch(
+      `${BASE_URL}/tv/${id}?api_key=${API_KEY}&append_to_response=videos,images`
     ).then((res) => res.json());
   },
 };
--- screens/Detail.js
+++ screens/Detail.js
@@ -1,27 +1,144 @@
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  Linking,
+  TouchableOpacity,
+  Share,
+  Platform,
+} from 'react-native';
+import { useQuery } from 'react-query';
 import styled from 'styled-components/native';
+import { moviesApi, tvApi } from '../api';
 import colors from '../colors';
 import Poster from '../components/Poster';
+import { makeImgPath } from '../utils';
+import { Ionicons } from '@expo/vector-icons';
+
+const { height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 const Detail = ({ navigation: { setOptions }, route: { params } }) => {
+  const isMovie = 'original_title' in params;
+
+  const { isLoading, data } = useQuery(
+    [isMovie ? 'movies' : 'tv', params.id],
+    isMovie ? moviesApi.detail : tvApi.detail
+  );
+
+  const shareMedia = async () => {
+    const isAndroid = Platform.OS === 'android';
+    const homepage = isMovie
+      ? `https://www.imdb.com/title/${data.imdb_id}`
+      : data.homepage;
+    if (isAndroid) {
+      await Share.share({
+        message: `${params.overview}\nCheck it out: ${homepage}`,
+        title: isMovie ? params.original_title : params.original_name,
+      });
+    } else {
+      await Share.share({
+        url: homepage,
+        title: isMovie ? params.original_title : params.original_name,
+      });
+    }
+  };
+  const ShareButton = () => (
+    <TouchableOpacity onPress={shareMedia}>
+      <Ionicons name="share-outline" color="white" size={24} />
+    </TouchableOpacity>
+  );
+
   useEffect(() => {
     setOptions({
-      title:
-        'original_title' in params
-          ? params.original_title
-          : params.original_name,
+      title: 'original_title' in params ? 'Movie' : 'TV Show',
     });
   }, []);
+
+  useEffect(() => {
+    if (data) {
+      setOptions({
+        headerRight: () => <ShareButton />,
+      });
+    }
+  }, [data]);
+
+  const openYTLink = async (videoID) => {
+    const baseUrl = `https://m.youtube.com/watch?v=${videoID}`;
+    await Linking.openURL(baseUrl);
+  };
+
   return (
     <Container>
-      <Poster path={params.poster_path || ''} />
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path || '') }}
+        />
+        <Column>
+          <Poster path={params.poster_path || ''} />
+          <Title>
+            {'original_title' in params
+              ? params.original_title
+              : params.original_name}
+          </Title>
+        </Column>
+      </Header>
+      <Data>
+        <Overview>{params.overview}</Overview>
+        {data?.videos.results?.map((video) => (
+          <VideoBtn key={video.key} onPress={() => openYTLink(video.key)}>
+            <Ionicons name="logo-youtube" color="white" size={24} />
+            <BtnText>{video.name}</BtnText>
+          </VideoBtn>
+        ))}
+      </Data>
     </Container>
   );
 };
 
 const Container = styled.ScrollView`
   background-color: ${colors.black};
+`;
+
+const Header = styled.View`
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0px 20px;
+`;
+
+const Background = styled.Image``;
+
+const Column = styled.View`
+  flex-direction: row;
+  width: 80%;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 36px;
+  align-self: flex-end;
+  margin-left: 15px;
+  font-weight: 500;
+`;
+
+const Data = styled.View`
+  padding: 0px 20px;
+`;
+
+const Overview = styled.Text`
+  color: white;
+  margin-top: 20px;
+  padding: 20px 0px;
+`;
+const VideoBtn = styled.TouchableOpacity`
+  flex-direction: row;
+`;
+const BtnText = styled.Text`
+  color: white;
+  font-weight: 600;
+  margin-bottom: 10px;
+  line-height: 24px;
+  margin-left: 10px;
 `;
 
 export default Detail;
